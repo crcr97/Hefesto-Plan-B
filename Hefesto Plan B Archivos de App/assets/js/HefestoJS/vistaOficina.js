96b87abf209597fcc65c371cb0a50a39f83f211f
crcr97 <37202907+crcr97@users.noreply.github.com>
2020-11-26 15:18:30 -0600

    App casi terminada
    App casi terminada

---
+++ Hefesto Plan B Archivos de App/assets/js/HefestoJS/vistaOficina.js
@@ -0,0 +1,13 @@
+const productosContainer = document.getElementById("oficinas-container");
+
+window.addEventListener("DOMContentLoaded", async (e) => {
+    const productoSeleccionado = productosContainer.querySelectorAll(".oficina-card");
+    productoSeleccionado.forEach((seleccion) => {
+        seleccion.addEventListener("click", async (e) => {
+            const doc = e.currentTarget.dataset.id;
+            localStorage.setItem("pantalla-anterior", "oficina");
+            localStorage.setItem("categoria-seleccionada", doc);
+            window.location.href = "vistaDetalleCategoria.html";
+        });
+    });
+});
